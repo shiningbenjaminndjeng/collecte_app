1a3e8c9dc216b3f70821b6965d2387565bfc41e0
shiningbenjaminndjeng <benjaminndjengshining@gmail.com>
2026-04-13 14:11:59 +0100

    deploy

--- collecte_app/setting.py
+++ collecte_app/setting.py
@@ -13,11 +13,12 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] 
 DATABASE_URL = 'postgresql://collecte_user:LRl03YV6T6SkcC0oK2kyIpYyCTJ8RC2w@dpg-d7ebetjbc2fs73efv6d0-a/collecte'
 
 import dj_database_url
 
 DATABASES = {
+    
     'default': dj_database_url.parse(DATABASE_URL)
 }

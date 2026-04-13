c1cf0367ace651ebf0bceacb4e734f9e62c8a376
SHINING <benjaminndjengshining@gmail.com>
2026-04-13 16:50:19 +0100

    Update wsgi.py

--- collecte_app/wsgi.py
+++ collecte_app/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collecte_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collecte_app')
 
 application = get_wsgi_application()
